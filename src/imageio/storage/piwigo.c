1c448e10e781d7032b14d3219f293c98574af664
Codeklöppler <development@consensionis.de>
2018-11-19 16:17:42 +0100

    moved defines and function prototypes to header file

--- src/imageio/storage/piwigo.c
+++ src/imageio/storage/piwigo.c
@@ -24,6 +24,9 @@
 * Beenden von Darktable                     -> gui_cleanup()
 
 */
+#include <curl/curl.h>
+#include <json-glib/json-glib.h>
+#include <stdlib.h>
 
 #include "bauhaus/bauhaus.h"
 #include "common/darktable.h"
@@ -40,129 +43,13 @@
 #include "gui/gtk.h"
 #include "gui/gtkentry.h"
 #include "imageio/storage/imageio_storage_api.h"
-#include <curl/curl.h>
-#include <json-glib/json-glib.h>
-#include <stdlib.h>
+#include "piwigo.h"
 
 #define DT_PIWIGO_DEBUG 1
-
-#define DT_PIWIGO_VERSION 0                 // module version
-#define DT_PIWIGO_NAME    "Piwigo"          // module name
-#define DT_PIWIGO_FAILED  FALSE
-#define DT_PIWIGO_SUCCESS TRUE
-#define DT_PIWIGO_NO      DT_PIWIGO_FAILED
-#define DT_PIWIGO_YES     DT_PIWIGO_SUCCESS
-
-#define DT_PWG_POSTDATA_MAXLEN 8000         // maximum length of POST data string length
-
-// No maximum dimension
-#define DT_PIWIGO_DIMENSION_MAX   0         
-// No recommended dimension
-#define DT_PIWIGO_DIMENSION_BEST  0         
-#define DT_PIWIGO_API     "/ws.php?format=json&method="
-#define DT_PIWIGO_SITE_MAXLEN ( 1024*sizeof(char) )
-#define DT_PIWIGO_METHOD_MAXLEN ( 8192*sizeof(char) ) 
-#define DT_PIWIGO_URL_MAXLEN ( DT_PIWIGO_SITE_MAXLEN + DT_PIWIGO_METHOD_MAXLEN )
-
-// Supported Piwigo API method sections
-#define DT_PIWIGO_PREFIX "pwg"
-#define DT_PIWIGO_SESSION DT_PIWIGO_PREFIX ".session"
-#define DT_PIWIGO_CATEGORIES DT_PIWIGO_PREFIX ".categories"
-#define DT_PIWIGO_IMAGES DT_PIWIGO_PREFIX ".images"
-
-#define DT_PIWIGO_API_SESSION_LOGIN DT_PIWIGO_API DT_PIWIGO_SESSION ".login"
-#define DT_PIWIGO_API_SESSION_LOGOUT DT_PIWIGO_API DT_PIWIGO_SESSION ".logout"
-#define DT_PIWIGO_API_SESSION_GET_STATUS DT_PIWIGO_API DT_PIWIGO_SESSION ".getStatus"
-#define DT_PIWIGO_API_CATEGORY_GETLIST DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".getList"
-
-#define DT_PIWIGO_API_CATEGORY_GET_ADMINLIST DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".getAdminList"
-
-#define DT_PIWIGO_API_CATEGORY_ADD DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".Add"
-
-// PIWIGO API METHOD OPTIONS
-#define PWG_GETLIST_RECURSIVE "&recursive=true"
-#define PWG_GETLIST_TREE "&tree_output=true"
 
 // module supports darktable release 2.x
 DT_MODULE(2)
 
-typedef enum piwigo_methods {
-  none = 0,
-  getStatus,
-  login,
-  logout,
-  addImages,
-  getCategories
-} piwigo_methods_t;
-
-typedef struct dt_module_imageio_storage_piwigo_auth
-{
-  gchar *username;
-  gboolean registered;
-  gchar *token;
-  gboolean sslPeer;
-} dt_module_imageio_storage_piwigo_auth_t;
-
-
-typedef struct dt_module_imageio_storage_piwigo_context
-{
-  dt_module_imageio_storage_piwigo_auth_t *auth;
-  CURL *curl;
-  char *site;
-  char *path;
-  dt_variables_params_t *params;
-  JsonParser *parser;
-  gchar *supported_file_types;
-} dt_module_imageio_storage_piwigo_context_t;
-
-
-typedef struct dt_module_imageio_storage_piwigo_ui
-{
-  GtkWidget *site;
-  GtkWidget *site_label;
-  GtkWidget *album;
-  GtkWidget *album_label;
-  GtkWidget *username;
-  GtkWidget *username_label;
-  GtkWidget *password;
-  GtkWidget *password_label;
-  GtkWidget *login_button;
-  dt_module_imageio_storage_piwigo_context_t *context;
-} dt_module_imageio_storage_piwigo_ui_t;
-
-// STATIC FUNCTION PROTOTYPES
-static void 
-pwg_init(dt_module_imageio_storage_piwigo_ui_t **ui);
-
-static void 
-clicked_login_button(GtkWidget *widget, dt_imageio_module_storage_t *self);
-
-static void 
-config_changed_callback(GtkEntry *entry, gpointer user_data);
-
-static size_t 
-curl_write_data_callback(void *ptr, size_t size, size_t nmemb, void *data);
-
-static JsonObject *
-parse_json_response(dt_module_imageio_storage_piwigo_context_t *context, GString *response);
-
-static gboolean 
-pwg_login(dt_module_imageio_storage_piwigo_ui_t *ui);
-
-static gboolean 
-pwg_logout(dt_module_imageio_storage_piwigo_ui_t *ui);
-
-static gboolean 
-pwg_call(dt_module_imageio_storage_piwigo_ui_t *ui, GString *response, const char* postdata, const char *method);
-
-static void 
-pwg_debug(const char *format, const char *file, size_t line, ...);
-
-static void 
-_finalize_store(gpointer user_data);
-
-//static int pwg_createCategoryPath(dt_module_imageio_storage_piwigo_ui_t *ui, const char * path);
-//static void ui_refresh_albums_fill(PicasaAlbum *album, GtkListStore *list_store);
 
 // STATIC FUNCTIONS IMPLEMENTATION
 /* allow the module to initialize itself */
@@ -490,7 +377,7 @@ void gui_init(struct dt_imageio_module_storage_t *self)
   gtk_entry_set_width_chars(GTK_ENTRY(ui->password), 0);
   
   // Handle as login button press, on enter key press.
-  g_signal_connect(ui->password, "activate", G_CALLBACK(clicked_login_button), self);
+  g_signal_connect(ui->password, "activate", G_CALLBACK(pwg_password_entry_activated), self);
   
   // disable darktable hotkeys, when password field has focus
   dt_gui_key_accel_block_on_focus_connect(GTK_WIDGET(ui->password));
@@ -498,7 +385,7 @@ void gui_init(struct dt_imageio_module_storage_t *self)
   // add login/logout button
   ui->login_button = gtk_button_new_with_label("Login");
   gtk_box_pack_start(GTK_BOX(vbox), GTK_WIDGET(ui->login_button), TRUE, TRUE, 0);
-  g_signal_connect(G_OBJECT(ui->login_button), "clicked", G_CALLBACK(clicked_login_button), self);
+  g_signal_connect(G_OBJECT(ui->login_button), "clicked", G_CALLBACK(pwg_login_button_clicked), self);
 
   // add album label
   ui->album_label = gtk_label_new(_("Album"));
@@ -1000,8 +887,13 @@ void export_dispatched(struct dt_imageio_module_storage_t *self)
   return;
 }
 
+// On <Return> inside the password entry widget, call login button press
+static void pwg_password_entry_activated(GtkWidget *widget, dt_imageio_module_storage_t *self)
+{
+  pwg_login_button_clicked(widget, self);
+}
 
-static void clicked_login_button(GtkWidget *widget, dt_imageio_module_storage_t *self)
+static void pwg_login_button_clicked(GtkWidget *widget, dt_imageio_module_storage_t *self)
 {
   dt_module_imageio_storage_piwigo_ui_t *ui = (dt_module_imageio_storage_piwigo_ui_t *) self->gui_data;
 
---
+++ src/imageio/storage/piwigo.h
@@ -0,0 +1,114 @@
+#define DT_PIWIGO_VERSION 0                 // module version
+#define DT_PIWIGO_NAME    "Piwigo"          // module name
+#define DT_PIWIGO_FAILED  FALSE
+#define DT_PIWIGO_SUCCESS TRUE
+#define DT_PIWIGO_NO      DT_PIWIGO_FAILED
+#define DT_PIWIGO_YES     DT_PIWIGO_SUCCESS
+
+#define DT_PWG_POSTDATA_MAXLEN 8000         // maximum length of POST data string length
+
+// No maximum dimension
+#define DT_PIWIGO_DIMENSION_MAX   0         
+// No recommended dimension
+#define DT_PIWIGO_DIMENSION_BEST  0         
+#define DT_PIWIGO_API     "/ws.php?format=json&method="
+#define DT_PIWIGO_SITE_MAXLEN ( 1024*sizeof(char) )
+#define DT_PIWIGO_METHOD_MAXLEN ( 8192*sizeof(char) ) 
+#define DT_PIWIGO_URL_MAXLEN ( DT_PIWIGO_SITE_MAXLEN + DT_PIWIGO_METHOD_MAXLEN )
+
+// Supported Piwigo API method sections
+#define DT_PIWIGO_PREFIX "pwg"
+#define DT_PIWIGO_SESSION DT_PIWIGO_PREFIX ".session"
+#define DT_PIWIGO_CATEGORIES DT_PIWIGO_PREFIX ".categories"
+#define DT_PIWIGO_IMAGES DT_PIWIGO_PREFIX ".images"
+
+#define DT_PIWIGO_API_SESSION_LOGIN DT_PIWIGO_API DT_PIWIGO_SESSION ".login"
+#define DT_PIWIGO_API_SESSION_LOGOUT DT_PIWIGO_API DT_PIWIGO_SESSION ".logout"
+#define DT_PIWIGO_API_SESSION_GET_STATUS DT_PIWIGO_API DT_PIWIGO_SESSION ".getStatus"
+#define DT_PIWIGO_API_CATEGORY_GETLIST DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".getList"
+
+#define DT_PIWIGO_API_CATEGORY_GET_ADMINLIST DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".getAdminList"
+
+#define DT_PIWIGO_API_CATEGORY_ADD DT_PIWIGO_API DT_PIWIGO_CATEGORIES ".Add"
+
+// PIWIGO API METHOD OPTIONS
+#define PWG_GETLIST_RECURSIVE "&recursive=true"
+#define PWG_GETLIST_TREE "&tree_output=true"
+
+typedef enum piwigo_methods {
+  none = 0,
+  getStatus,
+  login,
+  logout,
+  addImages,
+  getCategories
+} piwigo_methods_t;
+
+typedef struct dt_module_imageio_storage_piwigo_auth
+{
+  gchar *username;
+  gboolean registered;
+  gchar *token;
+  gboolean sslPeer;
+} dt_module_imageio_storage_piwigo_auth_t;
+
+
+typedef struct dt_module_imageio_storage_piwigo_context
+{
+  dt_module_imageio_storage_piwigo_auth_t *auth;
+  CURL *curl;
+  char *site;
+  char *path;
+  dt_variables_params_t *params;
+  JsonParser *parser;
+  gchar *supported_file_types;
+} dt_module_imageio_storage_piwigo_context_t;
+
+
+typedef struct dt_module_imageio_storage_piwigo_ui
+{
+  GtkWidget *site;
+  GtkWidget *site_label;
+  GtkWidget *album;
+  GtkWidget *album_label;
+  GtkWidget *username;
+  GtkWidget *username_label;
+  GtkWidget *password;
+  GtkWidget *password_label;
+  GtkWidget *login_button;
+  dt_module_imageio_storage_piwigo_context_t *context;
+} dt_module_imageio_storage_piwigo_ui_t;
+
+// STATIC FUNCTION PROTOTYPES
+static void 
+pwg_init(dt_module_imageio_storage_piwigo_ui_t **ui);
+
+static void 
+pwg_login_button_clicked(GtkWidget *widget, dt_imageio_module_storage_t *self);
+//pwg_clicked_login_button
+static void 
+config_changed_callback(GtkEntry *entry, gpointer user_data);
+
+static size_t 
+curl_write_data_callback(void *ptr, size_t size, size_t nmemb, void *data);
+
+static JsonObject *
+parse_json_response(dt_module_imageio_storage_piwigo_context_t *context, GString *response);
+
+static gboolean 
+pwg_login(dt_module_imageio_storage_piwigo_ui_t *ui);
+
+static gboolean 
+pwg_logout(dt_module_imageio_storage_piwigo_ui_t *ui);
+
+static gboolean 
+pwg_call(dt_module_imageio_storage_piwigo_ui_t *ui, GString *response, const char* postdata, const char *method);
+
+static void 
+pwg_debug(const char *format, const char *file, size_t line, ...);
+
+static void 
+_finalize_store(gpointer user_data);
+
+//static int pwg_createCategoryPath(dt_module_imageio_storage_piwigo_ui_t *ui, const char * path);
+//static void ui_refresh_albums_fill(PicasaAlbum *album, GtkListStore *list_store);
